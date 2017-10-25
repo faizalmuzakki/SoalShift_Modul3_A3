6a32ceb47f7a5cbbc78024cb7021e2d91cbd07d0
daniseyy <33064227+daniseyy@users.noreply.github.com>
2017-10-25 15:39:32 +0700

    Update No.4
    Pake fungsi atoi

--- nomer4.cpp
+++ nomer4.cpp
@@ -1,11 +1,36 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int factorial(int a){
-	if(a==0 || a==1) return 1;
-	else return a*factorial(a-1);
+int num;
+
+void* factorial(void* fac)
+{
+int fac=1;
+for(int a=1;a<=num;a++) 
+{
+fac=fac*a;
+printf("Hasil %d! = %d\n",num, fac);
 }
-int main(){
-	printf("%d", factorial(3));
-	return 0;
+}
+
+
+
+
+int main (int argc, char* argv[]){
+    pthread_t thread[argc];
+    int a, sommpen;
+    
+    for(a=1;a<argc;a++) {
+    
+    sommpen = atoi(argv[a])
+    pthread_create(thread[a], NULL,&factorial,(void*)sommpen);
+    }
+    
+    for(a=1;a<argc;a++){
+    pthread_join(thread[a],NULL);
+    }
+
 }
